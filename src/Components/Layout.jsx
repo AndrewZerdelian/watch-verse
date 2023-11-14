0da4de5bf9055c281bfd46ec9c38903a79df76c2
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-11-14 13:34:01 +0200

    installing react router dom

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <>
+    <Navbar/>
+    
+    <Footer/>
+    </>
   )
 }
