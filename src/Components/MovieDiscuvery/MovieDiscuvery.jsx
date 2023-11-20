9f463885956d177a05d329996134398b09359033
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-11-20 15:57:57 +0200

    ..

--- src/Components/MovieDiscuvery/MovieDiscuvery.jsx
+++ src/Components/MovieDiscuvery/MovieDiscuvery.jsx
@@ -5,7 +5,7 @@ import FirstImage2 from "../../Assets/2.jpg";
 import FirstImage3 from "../../Assets/3.jpg";
 import { useContext, useEffect, useState } from "react";
 import { MovieDiscuveryCont } from "../../Context/MovieDiscuveryContext/MovieDiscuveryContext";
-import axios from 'axios';
+import axios from "axios";
 export default function MovieDiscuvery() {
   var settings = {
     dots: true,
@@ -23,19 +23,14 @@ export default function MovieDiscuvery() {
           width: "30px",
           color: "black",
         }}
-      >
-        
-      </div>
+      ></div>
     ),
   };
 
-
   //////////////////////////////API FOR CAURSEL /////////////////////////
   const { MovieDiscuveryCarouselGetAPI } = useContext(MovieDiscuveryCont);
-  const [
-    MappingMovieDiscuveryContext,
-    setMappingMovieDiscuveryContext,
-  ] = useState([]);
+  const [MappingMovieDiscuveryContext, setMappingMovieDiscuveryContext] =
+    useState([]);
 
   async function GetDiscvuverMovies() {
     const response = await MovieDiscuveryCarouselGetAPI();
@@ -47,27 +42,47 @@ export default function MovieDiscuvery() {
     GetDiscvuverMovies();
   }, []);
 
-  ////////////////////////////////test //////////////////////////////////
- 
-
-const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/movie/872585/images',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDdiMjRkZmU5MGNiOTJiYWIyZjQyMDI2ZGRmOGRhNyIsInN1YiI6IjY0YzY0NTU1Y2FkYjZiMDBhYzY2MGI4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qQtJ_EechHLDtkmDGhavEXqInLrEpa57XMwUVmQLwok'
+  ////////////////////////////////test for movie images //////////////////////////////////
+  async function CoverImages() {
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/movie/872585/images"
+      );
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   }
-};
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-    
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+  useEffect(() => {
+    CoverImages();
+  }, []);
+
+  /**
+  *  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/872585/images",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDdiMjRkZmU5MGNiOTJiYWIyZjQyMDI2ZGRmOGRhNyIsInN1YiI6IjY0YzY0NTU1Y2FkYjZiMDBhYzY2MGI4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qQtJ_EechHLDtkmDGhavEXqInLrEpa57XMwUVmQLwok",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  */
+
+  ///////////////////////////////TO BE TESTED TOMORROW WITH THE API
+  //need to make it with async function try and catch and axios .get method
+
   return (
     <Slider {...settings} className="w-100">
       <div>
@@ -78,9 +93,6 @@ axios
       </div>
       <div>
         <img src={FirstImage3} alt="background" />
-      </div>
-      <div>
-        
       </div>
     </Slider>
   );
