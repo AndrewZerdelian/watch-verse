319e58608a40abd19d0933ab852dfcd52af516d4
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-12-22 15:32:43 +0200

    update

--- src/Components/Favourites/Favourites.jsx
+++ src/Components/Favourites/Favourites.jsx
@@ -2,12 +2,12 @@ import React, { useContext, useEffect } from "react";
 import { FavouritesContext } from "../../Context/FavouritesContext/FavouritesContext";
 
 export default function Favourites() {
-  const { GetFavouritesContext,  } = useContext(FavouritesContext);
+  const { GetFavouritesContext } = useContext(FavouritesContext);
 
   async function DiscplayingFavouritesFromContext() {
-    const response = await GetFavouritesContext();
-    console.log(response);
-    return response;
+    // const response = await GetFavouritesContext();
+    // console.log(response);
+    // return response;
   }
 
   useEffect(() => {
@@ -16,6 +16,30 @@ export default function Favourites() {
   return (
     <>
       <h1 className="text-center p-5 text-danger">Favourites List</h1>
+
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        class="modal fade bg-black"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content bg-dark">
+            <div class="modal-header"></div>
+            <div class="modal-body">...</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="card text-white bg-dark">
+      <div className="card text-white bg-black">
         <h5 className="card-header">Featured</h5>
         <div className="card-body">
           <h5 className="card-title">Special title treatment</h5>
@@ -11,7 +11,7 @@ export default function Footer() {
             With supporting text below as a natural lead-in to additional
             content.
           </p>
-          <a href="/" className="btn btn-primary">
+          <a href="/" className="btn btn-danger">
             Go somewhere
           </a>
         </div>
